e0d62cba5f3917e70880388cd7f63fcffa4a52b4
Bunty Raghani <bootcamptoprod@gmail.com>
2024-08-11 18:01:48 +0530

    Spring Data JPA print sql queries

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+## How to Print SQL Queries with Values in Spring Boot with Spring Data JPA
+For complete understanding on how we can print SQL queries with values in Spring Boot with Spring Data JPA you can check out our blog.
+<br/><br/>**Blog Link:** [How to Print SQL Queries with Values in Spring Boot with Spring Data JPA](https://bootcamptoprod.com/spring-boot-data-jpa-print-sql-queries/)
+<br/>
+
 # spring-boot-data-jpa-sql-queries-logging
 A simple spring boot app showcasing how to log spring data jpa sql queries in console
--- src/main/java/com/bootcamptoprod/SpringBootDataJpaSqlQueriesLoggingApplication.java
+++ src/main/java/com/bootcamptoprod/SpringBootDataJpaSqlQueriesLoggingApplication.java
@@ -1,13 +1,69 @@
 package com.bootcamptoprod;
 
+import com.bootcamptoprod.entity.User;
+import com.bootcamptoprod.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class SpringBootDataJpaSqlQueriesLoggingApplication {
+public class SpringBootDataJpaSqlQueriesLoggingApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDataJpaSqlQueriesLoggingApplication.class, args);
-	}
+    @Autowired
+    private UserRepository userRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataJpaSqlQueriesLoggingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        // Insert
+        System.out.println("\nStarting Insert Operations");
+
+        User user1 = new User();
+        user1.setName("John Doe");
+        user1.setEmail("john.doe@example.com");
+        userRepository.save(user1);
+
+        User user2 = new User();
+        user2.setName("Jane Doe");
+        user2.setEmail("jane.doe@example.com");
+        userRepository.save(user2);
+
+        System.out.println("\nInsert Operations Completed");
+
+       /* // Select
+        System.out.println("\nStarting Select All Users Operation");
+        List<User> users = userRepository.findAll();
+        users.forEach(System.out::println);*/
+
+        System.out.println("\nSelect All Users Operation Completed");
+
+        // Update
+        System.out.println("\nStarting Update Operation");
+        User userToUpdate = userRepository.findById(user1.getId()).orElseThrow();
+        userToUpdate.setEmail("new.john.doe@example.com");
+        userRepository.save(userToUpdate);
+
+        System.out.println("\nUpdate Operation Completed");
+
+        // Delete
+        System.out.println("\nStarting Delete Operation");
+        userRepository.deleteById(user2.getId());
+
+        System.out.println("\nDelete Operation Completed");
+
+
+        // Select with parameter
+        System.out.println("\nStarting Select with Parameter Operation");
+        List<User> johns = userRepository.findByName("John Doe");
+        johns.forEach(System.out::println);
+
+        System.out.println("\nSelect with Parameter Operation Completed");
+    }
 }
